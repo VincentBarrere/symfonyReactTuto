294483cda2bec1061a470d9e7358560d13ea2b85
VincentBarrere <vincentbarrere0@gmail.com>
2022-02-21 14:44:25 +0100

    init

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/blog', name: 'blog')]
+class BlogController extends AbstractController
+{
+    private const POSTS = [
+        [
+            'id' => 1,
+            "slug" => "hello-world",
+            "title" => "Hello World"
+        ],
+        [
+            'id' => 2,
+            "slug" => "another-post",
+            "title" => "This is another post"
+        ],
+        [
+            'id' => 2,
+            "slug" => "last-post",
+            "title" => "This is last post"
+        ]
+    ];
+
+    #[Route("/", name: "blog_list")]
+    public function list()
+    {
+        return new JsonResponse(self::POSTS);
+    }
+    #[Route("/{id}", name: "blog_by_id")]
+    public function post($id)
+    {
+        return new JsonResponse(
+            array_search($id, array_column(self::POSTS, 'id'))
+        );
+    }
+    #[Route("/{slug}", name: "blog_by_slug")]
+    public function postBySlug($slug)
+    {
+        return new JsonResponse(
+            array_search($slug, array_column(self::POSTS, 'slug'))
+        );
+    }
+}
