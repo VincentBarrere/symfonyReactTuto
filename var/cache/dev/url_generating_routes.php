6e11730e22815ed11541e502345848ecdb7c4f49
VincentBarrere <vincentbarrere0@gmail.com>
2022-02-23 09:33:22 +0100

    debug

--- var/cache/dev/url_generating_routes.php
+++ var/cache/dev/url_generating_routes.php
@@ -4,8 +4,8 @@
 
 return [
     '_preview_error' => [['code', '_format'], ['_controller' => 'error_controller::preview', '_format' => 'html'], ['code' => '\\d+'], [['variable', '.', '[^/]++', '_format', true], ['variable', '/', '\\d+', 'code', true], ['text', '/_error']], [], [], []],
-    'blogblog_list' => [['page'], ['page' => 5, '_controller' => 'App\\Controller\\BlogController::list'], [], [['variable', '/', '[^/]++', 'page', true], ['text', '/blog']], [], [], []],
-    'blogblog_by_id' => [['id'], ['_controller' => 'App\\Controller\\BlogController::post'], [], [['variable', '/', '[^/]++', 'id', true], ['text', '/blog/post']], [], [], []],
+    'blogblog_list' => [['page'], ['page' => 5, '_controller' => 'App\\Controller\\BlogController::list'], ['page' => '\\d+'], [['variable', '/', '\\d+', 'page', true], ['text', '/blog']], [], [], []],
+    'blogblog_by_id' => [['id'], ['_controller' => 'App\\Controller\\BlogController::post'], ['id' => '\\d+'], [['variable', '/', '\\d+', 'id', true], ['text', '/blog/post']], [], [], []],
     'blogblog_by_slug' => [['slug'], ['_controller' => 'App\\Controller\\BlogController::postBySlug'], [], [['variable', '/', '[^/]++', 'slug', true], ['text', '/blog/post']], [], [], []],
     'blogblog_add' => [[], ['_controller' => 'App\\Controller\\BlogController::add'], [], [['text', '/blog/add']], [], [], []],
     'default' => [[], ['_controller' => 'App\\Controller\\DefaultController::index'], [], [['text', '/default']], [], [], []],
